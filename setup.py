3049a3451d9c5651c7be1596ddaa69e0051f83c8
Mark R. Tuttle <mrtuttle@amazon.com>
2022-02-16 20:24:57 +0000

    Advance version to 2.10

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cbmc-viewer",
-    version="2.9",
+    version="2.10",
     author="Mark R. Tuttle",
     author_email="mrtuttle@amazon.com",
     description="A CBMC viewer",
