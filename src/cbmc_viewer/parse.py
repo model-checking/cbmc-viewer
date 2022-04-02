0639f6417d0ccc4d65c480803079ced5b96d4baf
Mark R. Tuttle <mrtuttle@amazon.com>
2022-04-02 17:56:45 -0400

    Parse json files as files and not strings
    Viewer previously handled parsing json output of goto-analyzer as a
    special case, removing a prefix of goto-analyzer status messages that
    preceded the json.  A prior commit changes the invocation of
    goto-analyzer, and this is no longer necessary.

--- src/cbmc_viewer/parse.py
+++ src/cbmc_viewer/parse.py
@@ -8,7 +8,6 @@ from xml.etree import ElementTree
 
 import json
 import logging
-import re
 
 def parse_xml_file(xfile):
     """Parse an xml file."""
@@ -28,34 +27,21 @@ def parse_xml_string(xstr):
         logging.debug("%s", err)
         raise UserWarning(f"Can't parse xml string '{xstr[:40]}...'") from None
 
-def parse_json_file(jfile, goto_analyzer=False):
+def parse_json_file(jfile):
     """Parse an json file."""
 
     try:
         with open(jfile) as data:
-            if goto_analyzer:
-                return parse_json_string(data.read(), goto_analyzer)
             return json.load(data)
-    except (IOError, json.JSONDecodeError, UserWarning) as err:
+    except (IOError, json.JSONDecodeError) as err:
         logging.debug("%s", err)
         raise UserWarning(f"Can't load json file '{jfile}' in {Path.cwd()}") from None
 
-def parse_json_string(jstr, goto_analyzer=False):
+def parse_json_string(jstr):
     """Parse a json string."""
 
     try:
-        jstr = clean_up_goto_analyzer(jstr) if goto_analyzer else jstr
         return json.loads(jstr)
     except json.JSONDecodeError as err:
         logging.debug("%s", err)
         raise UserWarning(f"Can't parse json string '{jstr[:40]}...'") from None
-
-def clean_up_goto_analyzer(json_data):
-    """Clean up the json output of goto-analyzer."""
-
-    # the json produced by goto-analyzer is preceeded by a block of text
-    json_data = re.sub(r'^.*\n\[', '[', json_data, flags=re.DOTALL)
-    # the json produced by goto-analyzer sometimes omits line numbers
-    json_data = json_data.replace('"first line": ,\n', '"first line": 0,\n')
-    json_data = json_data.replace('"last line": \n', '"last line": 0\n')
-    return json_data
