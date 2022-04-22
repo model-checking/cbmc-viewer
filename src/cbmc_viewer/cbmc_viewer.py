16adcdda382cbca4359c1a705b8ee4710d3867aa
Mark R. Tuttle <mrtuttle@amazon.com>
2022-04-22 16:31:42 -0400

    Ensure MIT-0 license in all source files

--- src/cbmc_viewer/cbmc_viewer.py
+++ src/cbmc_viewer/cbmc_viewer.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """Main entry point for cbmc-viewer and subcommands."""
 
 from cbmc_viewer import optionst
--- src/cbmc_viewer/etc/bash_completion.d/cbmc-viewer.sh
+++ src/cbmc_viewer/etc/bash_completion.d/cbmc-viewer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 ################################################################
 # Documentation
 #
